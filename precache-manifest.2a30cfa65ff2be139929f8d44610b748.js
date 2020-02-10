912b1d56ef81d663c5fcb6ed4303f4a43fcd6184
shubhamsingh11 <shubham11singh1997@gmail.com>
2020-02-10 21:31:44 +0530

    Updates

---
+++ precache-manifest.2a30cfa65ff2be139929f8d44610b748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f215145a78fbd903e5561fe6e4c0cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2f18efc66b66ab7a590",
+    "url": "/static/css/main.e130b171.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f18efc66b66ab7a590",
+    "url": "/static/js/main.6a328a9a.chunk.js"
+  },
+  {
+    "revision": "863018210bf1c6ae9c68",
+    "url": "/static/js/runtime-main.215bd54d.js"
+  }
+]);
